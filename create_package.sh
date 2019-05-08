38bfea17e9028af6f641b8467adee935d2da2fc2
mntyfrsh <ryan@prosoundsystem.com>
2019-05-08 06:36:44 +0000

    testing

--- create_package.sh
+++ create_package.sh
@@ -2,15 +2,22 @@
 
 VERSION="1.0.0"
 
+if [ ! -d "node_mdules" ]; then
+	echo 
+	echo "Modules must be compiled using install_node_modules.sh before running this script"
+	echo
+	exit 1
+fi
+
 # create temp directory
 mkdir -p /tmp/na-build/debian/DEBIAN
 mkdir -p /tmp/na-build/debian/DEBIAN/opt/navcoin-angular
 mkdir -p /tmp/na-build/debian/DEBIAN/etc/systemd/system
 
 # copy files
-cp -p debian_files/DEBIAN/* /tmp/na-build/debian/DEBIAN
-cp -p debian_files/DEBIAN/etc/systemd/system/* /tmp/na-build/debian/DEBIAN/etc/systemd/system
-cp -pr `pwd`/. /tmp/na-build/debian/DEBIAN/opt/navcoin-angular
+cp -p debian_files/DEBIAN/* /tmp/na-build/debian/DEBIAN/
+cp -p debian_files/DEBIAN/etc/systemd/system/* /tmp/na-build/debian/DEBIAN/etc/systemd/system/
+cp -pr `pwd`/. /tmp/na-build/debian/DEBIAN/opt/navcoin-angular/
 
 # build package
 cd /tmp/na-build/
