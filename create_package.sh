3fffe3d0d0982c9acccf4c5a0f46c60a1661b8c9
mntyfrsh <ryan@prosoundsystem.com>
2019-05-08 06:30:27 +0000

    Testing

--- create_package.sh
+++ create_package.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-checkinstall -D -y --maintainer "info@navcoin.org" --pkgname navcoin-angular --pkgversion 1.0 --include=navdroid/navdroid_files --install=no install.sh
+VERSION="1.0.0"
+
+# create temp directory
+mkdir -p /tmp/na-build/debian/DEBIAN
+mkdir -p /tmp/na-build/debian/DEBIAN/opt/navcoin-angular
+mkdir -p /tmp/na-build/debian/DEBIAN/etc/systemd/system
+
+# copy files
+cp -p debian_files/DEBIAN/* /tmp/na-build/debian/DEBIAN
+cp -p debian_files/DEBIAN/etc/systemd/system/* /tmp/na-build/debian/DEBIAN/etc/systemd/system
+cp -pr `pwd`/. /tmp/na-build/debian/DEBIAN/opt/navcoin-angular
+
+# build package
+cd /tmp/na-build/
+dpkg --build debian
+mv debian.deb /tmp/navcoin-angular_$VERSION.deb
+
+echo
+echo "Package is ready at /tmp/navcoin-angular_$VERSION.deb"
+echo
+echo
